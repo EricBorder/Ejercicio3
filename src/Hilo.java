7faa3aa6f519d84e9d926209749a35c549089611
EricBorder <magicsergy@gmail.com>
2022-10-14 10:25:36 +0200

    Terminado

--- src/Hilo.java
+++ src/Hilo.java
@@ -1,16 +1,25 @@
 public class Hilo extends Thread {
+    public static int contadorIni;
 
     public Hilo(String str) {
         super(str);
+        contadorIni++;
     }
 
     public void run() {
-        Hilo h = new Hilo("Prueba " );
-        h.start();
-
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Hilo " + getName() + i);
+        System.out.println("Hilo " + getName() + " Iniciado");
+        if(contadorIni<5){
+            new Hilo("Hilo " + contadorIni).start();
+            try {
+                Hilo.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
+        /*for (int i = 0; i < 5; i++) {
+            System.out.println(i + " " + getName());
 
+        }*/
+        System.out.println("Hilo " + getName() + " Finalizado");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-     Thread h = new Hilo("Hola");
-     h.start();
+        Thread h = new Hilo("Prueba");
+        h.start();
     }
 
 }
